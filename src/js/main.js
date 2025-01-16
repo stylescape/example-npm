082e7bc3fc2a11795a6fd06fb277a5b3b04d2a15
Lars van Vianen <lars@vianen.com>
2025-01-16 07:24:26 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-// Import the Stylescape bundle
-//
-// This includes Popper and all of Stylescape's JS plugins.
+// // Import the Stylescape bundle
+// //
+// // This includes Popper and all of Stylescape's JS plugins.
 
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
-//
-// Place any custom JS here
-//
+// //
+// // Place any custom JS here
+// //
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+// // Create an example popover
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//     .forEach(popover => {
+//         new bootstrap.Popover(popover)
+//     })
